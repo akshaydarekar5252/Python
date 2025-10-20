d279a39ffc30b8e898cf740751caed44565b57e6
Akshay Darekar <akshaydarekar5252@gmail.com>
2025-10-20 20:21:58 +0530

    sloved problems

---
+++ 02_conditionals/4_que.py
@@ -0,0 +1,15 @@
+# fruit ripeness checker 
+
+fruit ="banana" 
+color = "yellow"
+
+
+if fruit == "banana":
+    if color == "green":
+        print("unripe")
+    elif color == "yellow":
+        print("ripe ready to eat")
+    elif color == "brown":
+        print("overripe")
+else:
+    print("i dont have an idea about this fruit")
